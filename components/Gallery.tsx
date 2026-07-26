5dbfb108dbba47bd6c8b3f28b661429df3942793
Youll133 <youlles133@gmail.com>
2026-07-26 20:52:11 +0200

    Correction des bugs et mise a jour du site

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination, Zoom } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/zoom";
 
 export default function Gallery({
   images,
@@ -14,37 +15,25 @@ export default function Gallery({
 }) {
   return (
     <Swiper
-      modules={[Navigation, Pagination]}
-      navigation
-      pagination={{ clickable: true }}
-      spaceBetween={20}
-      slidesPerView={1}
-      style={{
-        width: "100%",
-        borderRadius: "20px",
-      }}
-    >
+  modules={[Navigation, Pagination, Zoom]}
+  navigation
+  pagination={{ clickable: true }}
+  zoom={true}
+  spaceBetween={20}
+  slidesPerView={1}
+  style={{
+    width: "100%",
+    borderRadius: "20px",
+  }}
+>
       {images.map((img, index) => (
         <SwiperSlide key={index}>
-          <div
-            style={{
-              width: "100%",
-              background: "#f3f4f6",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "20px",
-              overflow: "hidden",
-              minHeight: "300px",
-              maxHeight: "550px",
-            }}
-          >
+          <div className="swiper-zoom-container">
             <img
               src={img}
               alt={`Photo ${index + 1}`}
               style={{
                 width: "100%",
-                height: "100%",
                 maxHeight: "550px",
                 objectFit: "contain",
                 display: "block",
